e61c01b1b48d04377f4cd1d06c1b90b13459eee7
Michael Gouveia <mtgouveia06@gmail.com>
2025-03-04 10:43:43 -0500

    Upload Project

---
+++ Gouveia-Michael-a3-game/Game.cs
@@ -0,0 +1,30 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D;
+
+//Upload to Github!
+public class Game
+{
+    // Place your variables here:
+
+
+    /// <summary>
+    ///     Setup runs once before the game loop begins.
+    /// </summary>
+    public void Setup()
+    {
+
+    }
+
+    /// <summary>
+    ///     Update runs every frame.
+    /// </summary>
+    public void Update()
+    {
+
+    }
+}
+
